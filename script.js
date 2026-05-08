3c11d9e2bddd7aa73e0e913b9d786b59e1dbd064
Drew <81813325+drew-codes-things@users.noreply.github.com>
2026-05-08 14:34:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMenu() {
+    alert("Menu Opened! (This is a test result)");
+}
+
+document.getElementById('applicationForm').addEventListener('submit', function(e) {
+    const email = document.getElementById('email').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (!email.includes('@')) {
+        e.preventDefault(); 
+        errorMessage.style.display = 'block'; 
+    } else {
+        errorMessage.style.display = 'none';
+        alert('Thank you for applying, the process has started');
+    }
+});
